7281ad936bded991a8291733df624910321efe8f
CZW123445 <126428542+CZW123445@users.noreply.github.com>
2024-04-12 10:48:03 +0800

    Update StackFactory.java

--- 05-parenthesis-checker/src/main/java/oy/tol/tra/StackFactory.java
+++ 05-parenthesis-checker/src/main/java/oy/tol/tra/StackFactory.java
@@ -1,23 +1,26 @@
 package oy.tol.tra;
 
+
 public class StackFactory {
 
-   private StackFactory() {
-   }
+    private StackFactory() {
+    }
+   
+    public static StackInterface<Integer> createIntegerStack() {
+        return new StackImplementation<>();
+    }
 
-   public static StackInterface<Integer> createIntegerStack() {
-       return new StackImplementation<>();
-   }
+   
+    public static StackInterface<Integer> createIntegerStack(int capacity) {
+        return new StackImplementation<>(capacity);
+    }
 
-   public static StackInterface<Integer> createIntegerStack(int capacity) {
-       return new StackImplementation<>(capacity);
-   }
+    public static StackInterface<Character> createCharacterStack() {
+        return new StackImplementation<>();
+    }
 
-   public static StackInterface<Character> createCharacterStack() {
-       return new StackImplementation<>();
-   }
-
-   public static StackInterface<Character> createCharacterStack(int capacity) {
-       return new StackImplementation<>(capacity);
-   }
+  
+    public static StackInterface<Character> createCharacterStack(int capacity) {
+        return new StackImplementation<>(capacity);
+    }
 }
